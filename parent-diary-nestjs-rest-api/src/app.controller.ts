de58ca5e9105f57f391cfc6082ed2cae0beb3d64
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-05-09 22:55:28 +0400

    feat(config): mysql image plus config and sequelize install

--- parent-diary-nestjs-rest-api/src/app.controller.ts
+++ parent-diary-nestjs-rest-api/src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+
+import { Sequelize } from 'sequelize-typescript';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+    private sequelize: Sequelize,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello() + this.configService.get('db.host');
   }
 }
--- parent-diary-nestjs-rest-api/src/app.module.ts
+++ parent-diary-nestjs-rest-api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'parent_diary',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
