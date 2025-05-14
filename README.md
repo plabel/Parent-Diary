d12150275253e87f4b07205160b5f6df5284353f
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-05-14 14:48:48 +0400

    feat(signin): alert collapse transition

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Parent-Diary
 TODO
 - Signin
-  - validation inputs FE
-  - after form submit loading... and disabled while loading
-  - css transition when showing the alert
   - gestion des erreurs
     - field red when invalid and cant submit (use zod)
     - if duplicate email show custom err message explaining duplicate email
--- parent-diary-nextjs-web-app/src/app/_global/alert/alert-wrapper.tsx
+++ parent-diary-nextjs-web-app/src/app/_global/alert/alert-wrapper.tsx
@@ -1,17 +1,30 @@
+import { Collapse } from "react-bootstrap";
 import Alert from "react-bootstrap/esm/Alert";
 
 type AlertWrapperProps = {
-    variant: string;
-    message: string;
-    setShow: (show: boolean) => void;
-    show: boolean;
-}
+  variant: string;
+  message: string;
+  setShow: (show: boolean) => void;
+  show: boolean;
+};
 
-export default function AlertWrapper({ variant, message, setShow, show }: AlertWrapperProps) {
+export default function AlertWrapper({
+  variant,
+  message,
+  setShow,
+  show,
+}: AlertWrapperProps) {
   if (!show) return <></>;
-  return (
-    <Alert className="fixed-top" onClose={() => setShow(false)} variant={variant} dismissible>
-      {message}
-    </Alert>
+  else return (
+    <Collapse appear={true} in={show}>
+      <Alert
+        className="fixed-top"
+        onClose={() => setShow(false)}
+        variant={variant}
+        dismissible
+      >
+        {message}
+      </Alert>
+    </Collapse>
   );
 }
