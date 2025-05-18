0efa4c762f678c2e4e76a604976b2eb8c76e7edd
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-05-18 20:39:57 +0400

    test(logout) unit test logout helper

---
+++ parent-diary-nextjs-web-app/__tests__/helpers/login/submitLogout.fixtures.ts
@@ -0,0 +1,45 @@
+type SubmitLogOutTestCase = {
+  description: string;
+  fetchWrapperMockResult: {
+    data: boolean | null;
+    error: unknown | null;
+  };
+  showAlertNbOfCalls: number;
+  showAlertArgs: [string, string];
+};
+
+export const testCases: SubmitLogOutTestCase[] = [
+  {
+    description: `
+      Given the submitLogOut function is called
+      Then the showAlert function is called with the correct arguments
+    `,
+    fetchWrapperMockResult: {
+      data: true,
+      error: null,
+    },
+    showAlertNbOfCalls: 1,
+    showAlertArgs: [
+      "success",
+      "Log out successful",
+    ],
+  },
+  {
+    description: `
+      Given a valid form data
+      When the submitLogIn function is called but the fetchWrapper returns an error
+      Then the showAlert function is called with the correct arguments
+    `,
+    fetchWrapperMockResult: {
+      data: false,
+      error: {
+        message: "Log out failed",
+      },
+    },
+    showAlertNbOfCalls: 1,
+    showAlertArgs: [
+      "danger",
+      "Log out failed",
+    ],
+  },
+];
--- parent-diary-nextjs-web-app/__tests__/helpers/login/submitLogout.test.ts
+++ parent-diary-nextjs-web-app/__tests__/helpers/login/submitLogout.test.ts
@@ -1,6 +1,6 @@
-import { submitLogIn } from "@/app/login/helpers/submitLogIn";
+import { submitLogOut } from "@/app/login/helpers/submitLogout";
 import { fetchWrapper } from "@/app/_global/helpers/fetchWrapper";
-import { testCases } from "./submitLogIn.fixtures";
+import { testCases } from "./submitLogout.fixtures";
 jest.mock('@/app/_global/helpers/fetchWrapper', () => ({
     fetchWrapper: jest.fn(),
 }));
@@ -8,11 +8,11 @@ jest.mock('next/navigation', () => ({
     redirect: jest.fn(),
 }));
 
-describe('submitLogIn', () => {
-    test.each(testCases)('$description', async ({fetchWrapperMockResult, showAlertNbOfCalls, showAlertArgs, formData}) => {
+describe('submitLogOut', () => {
+    test.each(testCases)('$description', async ({fetchWrapperMockResult, showAlertNbOfCalls, showAlertArgs}) => {
         (fetchWrapper as jest.Mock).mockResolvedValueOnce(fetchWrapperMockResult);
         const showAlertFn = jest.fn();
-        await submitLogIn(formData, showAlertFn, ()=>{}, {}, ()=>{});
+        await submitLogOut(showAlertFn);
         if(showAlertNbOfCalls > 0) expect(showAlertFn).toHaveBeenCalledWith(...showAlertArgs);
     });
 });
