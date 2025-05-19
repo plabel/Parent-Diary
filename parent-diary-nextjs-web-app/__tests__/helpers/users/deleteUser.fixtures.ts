0241d0d13f201720717ff665d2b4b6b3e59d28df
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-05-19 09:08:36 +0400

    test(delete-user): unit test next delete user helper

---
+++ parent-diary-nextjs-web-app/__tests__/helpers/users/deleteUser.fixtures.ts
@@ -0,0 +1,44 @@
+type DeleteUserTestCase = {
+  description: string;
+  fetchWrapperMockResult: {
+    data: boolean | null;
+    error: unknown | null;
+  };
+  showAlertNbOfCalls: number;
+  showAlertArgs: [string, string];
+};
+
+export const testCases: DeleteUserTestCase[] = [
+  {
+    description: `
+      Given the deleteUser function is called
+      Then the showAlert function is called with the correct arguments
+    `,
+    fetchWrapperMockResult: {
+      data: true,
+      error: null,
+    },
+    showAlertNbOfCalls: 1,
+    showAlertArgs: [
+      "success",
+      "User deleted successfully",
+    ],
+  },
+  {
+    description: `
+      Given the deleteUser function is called but the fetchWrapper returns an error
+      Then the showAlert function is called with the correct arguments
+    `,
+    fetchWrapperMockResult: {
+      data: false,
+      error: {
+        message: "User deletion failed",
+      },
+    },
+    showAlertNbOfCalls: 1,
+    showAlertArgs: [
+      "danger",
+      "User deletion failed",
+    ],
+  },
+];
---
+++ parent-diary-nextjs-web-app/__tests__/helpers/users/deleteUser.test.ts
@@ -0,0 +1,18 @@
+import { deleteUser } from "@/app/_users/helpers/deleteUser";
+import { fetchWrapper } from "@/app/_global/helpers/fetchWrapper";
+import { testCases } from "./deleteUser.fixtures";
+jest.mock('@/app/_global/helpers/fetchWrapper', () => ({
+    fetchWrapper: jest.fn(),
+}));
+jest.mock('next/navigation', () => ({
+    redirect: jest.fn(),
+}));
+
+describe('deleteUser', () => {
+    test.each(testCases)('$description', async ({fetchWrapperMockResult, showAlertNbOfCalls, showAlertArgs}) => {
+        (fetchWrapper as jest.Mock).mockResolvedValueOnce(fetchWrapperMockResult);
+        const showAlertFn = jest.fn();
+        await deleteUser(showAlertFn);
+        if(showAlertNbOfCalls > 0) expect(showAlertFn).toHaveBeenCalledWith(...showAlertArgs);
+    });
+});
