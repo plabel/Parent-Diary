272f532bf0a8c5c7c95b5775e804c4b5181b26e4
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-05-27 08:46:36 +0400

    feat(header): add AppHeader component for navigation
    - Introduces AppHeader component to encapsulate the navigation bar functionality.
    - Replaces inline navigation in the home page with the new AppHeader for better code organization and reusability.
    - Implements user session verification and logout functionality within the header.

---
+++ parent-diary-nextjs-web-app/src/app/_global/app-header.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import DeleteUserModal from "../_users/delete-user-modal";
+import { submitLogOut } from "../login/helpers/submitLogout";
+import { useAlert } from "../_global/alert/alert-provider";
+import { useEffect, useState } from "react";
+import { verifySession } from "./helpers/verifySession";
+
+
+export default function AppHeader() {
+    const [_userId, setUserId] = useState<string | null>(null);
+    useEffect(() => {
+      const fetchUserId = async () => {
+        const userId = await verifySession();
+        setUserId(userId);
+      };
+      fetchUserId();
+    }, []);
+    const showAlert = useAlert();
+    return (
+        <header>
+        <Navbar expand="lg" className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <Container>
+            <Navbar.Brand href="#home">Parent Diary</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link href="/log-entry">New log entry</Nav.Link>
+                <NavDropdown title="User" id="basic-nav-dropdown">
+                  <NavDropdown.Item onClick={() => submitLogOut(showAlert)}>
+                    Log out
+                  </NavDropdown.Item>
+                  <DeleteUserModal />
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+}
--- parent-diary-nextjs-web-app/src/app/home/page.tsx
+++ parent-diary-nextjs-web-app/src/app/home/page.tsx
@@ -1,45 +1,11 @@
 "use client";
-import { useState, useEffect } from "react";
-import { verifySession } from "../_global/helpers/verifySession";
-import { submitLogOut } from "../login/helpers/submitLogout";
-import { useAlert } from "../_global/alert/alert-provider";
-import DeleteUserModal from "../_users/delete-user-modal";
 import LogEntries from "../_log-entry/log-entries";
-import { Nav, NavDropdown } from "react-bootstrap";
-import { Navbar } from "react-bootstrap";
-import { Container } from "react-bootstrap";
+import AppHeader from "../_global/app-header";
+
 export default function LogIn() {
-  const showAlert = useAlert();
-  const [_userId, setUserId] = useState<string | null>(null);
-  useEffect(() => {
-    const fetchUserId = async () => {
-      const userId = await verifySession();
-      setUserId(userId);
-    };
-    fetchUserId();
-  }, []);
   return (
     <>
-      <header>
-        <Navbar expand="lg" className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <Container>
-            <Navbar.Brand href="#home">Parent Diary</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/log-entry">New log entry</Nav.Link>
-                <NavDropdown title="User" id="basic-nav-dropdown">
-                  <NavDropdown.Item onClick={() => submitLogOut(showAlert)}>
-                    Log out
-                  </NavDropdown.Item>
-                  <DeleteUserModal />
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
+      <AppHeader />
       <main className={`w-100 m-auto`}>
         <div className="container pt-5">
           <LogEntries />
