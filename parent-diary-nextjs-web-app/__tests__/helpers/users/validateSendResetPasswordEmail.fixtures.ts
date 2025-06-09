66b733fc1b26cbf9126e3203aca7fb3699fbdbc8
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-06-09 10:26:46 +0400

    feat(tests): add validation test cases for send reset password email
    - Introduces test cases for validating the send reset password email functionality, covering scenarios for both valid and invalid email inputs.
    - Adds a new fixture file and test file to enhance test coverage for the email validation logic.

---
+++ parent-diary-nextjs-web-app/__tests__/helpers/users/validateSendResetPasswordEmail.fixtures.ts
@@ -0,0 +1,35 @@
+export type ValidateSendResetPasswordEmailTestCase = {
+    description: string;
+    input: unknown;
+    expected: {
+        formErrors: Record<string, boolean>;
+        isValid: boolean;
+    };
+}
+
+export const testCases: ValidateSendResetPasswordEmailTestCase[] = [
+    {
+        description: "should validate send reset password email",
+        input: {
+            email: "test@test.com",
+        },
+        expected: {
+            formErrors: {
+                email: false,
+            },
+            isValid: true,
+        },
+    },
+    {
+        description: "should not validate send reset password email, if email is not valid",
+        input: {
+            email: "test",
+        },
+        expected: {
+            formErrors: {
+                email: true,
+            },
+            isValid: false,
+        },
+    },
+]
---
+++ parent-diary-nextjs-web-app/__tests__/helpers/users/validateSendResetPasswordEmail.test.ts
@@ -0,0 +1,9 @@
+import { validateSendResetPasswordEmail } from "@/app/users/send-reset-password-email/helpers/sendResetPasswordEmail.validation";
+import { testCases } from "./validateSendResetPasswordEmail.fixtures";
+
+describe('validateSendResetPasswordEmail', () => {
+    test.each(testCases)('$description', ({input, expected}) => {
+        const result = validateSendResetPasswordEmail(input);
+        expect(result).toEqual(expected);
+    });
+});
