f675035386ebc4097c3ababc85b0dae95c32c664
Pierre Label <pierrelabel@pierre-label-macbookpro.home>
2025-06-10 11:50:26 +0400

    feat(tests): add family member deletion test cases
    - Introduces test cases for the family member deletion functionality, covering scenarios for successful deletion and error handling.
    - Adds a new fixture file and test file to enhance test coverage for the family member deletion logic.

--- parent-diary-nextjs-web-app/__tests__/helpers/family-members/deleteFamilyMember.test.ts
+++ parent-diary-nextjs-web-app/__tests__/helpers/family-members/deleteFamilyMember.test.ts
@@ -1,15 +1,15 @@
 import { fetchWrapper } from "@/app/_global/helpers/fetchWrapper";
-import { deleteLogEntry } from "@/app/_log-entry/helpers/deleteLogEntry";
-import { testCases } from "./deleteLogEntry.fixtures";
+import { deleteFamilyMember } from "@/app/_family-members/helpers/deleteFamilyMember";
+import { testCases } from "./deleteFamilyMember.fixtures";
 jest.mock('@/app/_global/helpers/fetchWrapper', () => ({
     fetchWrapper: jest.fn(),
 }));
 
-describe('deleteLogEntry', () => {
+describe('deleteFamilyMember', () => {
     test.each(testCases)('$description', async ({fetchWrapperMockResult, showAlertArgs}) => {
         (fetchWrapper as jest.Mock).mockResolvedValueOnce(fetchWrapperMockResult);
         const showAlertFn = jest.fn();
-        await deleteLogEntry(1, ()=>{}, showAlertFn, ()=>{});
+        await deleteFamilyMember(1, ()=>{}, showAlertFn, ()=>{});
         expect(showAlertFn).toHaveBeenCalledWith(...showAlertArgs);
     });
 });
